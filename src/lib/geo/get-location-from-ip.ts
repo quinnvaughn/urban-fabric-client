5a344a51cfa13784d3372f5296c7582897686ebb
Quinn Vaughn <qvaughn3@gmail.com>
2026-03-22 16:51:00 -0700

    Console log to prod so I can see what ip returns.

--- src/lib/geo/get-location-from-ip.ts
+++ src/lib/geo/get-location-from-ip.ts
@@ -8,11 +8,13 @@ export const getLocationFromIp = createServerFn({ method: "GET" }).handler(
 		const DEFAULT: LatLng = { lat: 34.0195, lng: -118.4912 } // Santa Monica
 		const ip =
 			getRequestHeader("x-forwarded-for") ?? getRequestHeader("x-real-ip") ?? ""
+		console.log("Client IP address", ip)
 		if (!ip) return DEFAULT
 		try {
 			const res = await fetch(
 				`https://api.ipwho.org/${ip}?apiKey=${process.env.IP_WHO_KEY}`,
 			)
+			console.log("IP Geolocation response", res)
 			const json = await res.json()
 			if (typeof json.lat !== "number" || typeof json.lon !== "number") {
 				return DEFAULT
